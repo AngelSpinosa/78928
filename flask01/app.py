863f973325600b9cc45835528fc1f3c8c62d957b
AngelSpinosa <133794141+AngelSpinosa@users.noreply.github.com>
2025-03-07 10:58:39 -0600

    Cambios en flask

--- flask01/app.py
+++ flask01/app.py
@@ -3,5 +3,19 @@ app = Flask (__name__)
 @app.route('/')
 def hola_mundo():
     return 'Hola Mundo'
+    
+@app.route('/hola')
+def hola_html():
+    return '<h1 style="color:red;">Hola Mundo</h1>'
+    
+@app.route('/json')
+def algo():
+    return '{"nombre":"john}'
+
+app route('/xml')
+def xml():
+    return '<?xml version="1.0" encoding="UTF-8" ?><nombre>John</nombre>'
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0'
+debug=True)
